85a9df7ec236913bde2803c525fd84a96ec7a621
“Andrew <abiv23@gmail.com>
2017-02-04 15:55:43 -0700

    added some functionality

--- src/app/message-input/message-input.component.js
+++ src/app/message-input/message-input.component.js
@@ -1,5 +1,28 @@
 import template from './message-input.template.html';
 
+const controller = function() {
+    const vm = this
+
+    vm.$onInit = function() {
+
+    }
+
+    vm.sendMessage = function() {
+        //we will need a global user object with (url, name, messge)
+        let message = $('.message-area').val(); //
+        console.log(message);
+        let data = {
+            name: 'Mike',
+            message: message,
+            url: 'testURL'
+        }
+        socket.emit('send-message', data)
+
+    }
+
+}
+
 module.exports = {
-  template
+    template,
+    controller
 }
