23c1f5612f495218bd4f52bb8750f9adef435a6c
Mike Dowd <mikedowd1972@gmail.com>
2017-02-06 16:37:09 -0700

    worked on the user list feature

--- src/app/users-list/users-list.component.js
+++ src/app/users-list/users-list.component.js
@@ -10,19 +10,14 @@ const controller = function(socket, $scope) {
   vm.currentUser = {name: "mike", socketId: 'asfscfs23'};
 
   vm.$onInit = function() { //This needs to happen every time the users url changes. Is onInit the proper place for that?
+    console.log('this is an init');
     socket.emit('get-users', vm.currentUser);
     // This is a request to sockets to gather the address of every one with currentUsers url and send back an array of them
 
-    socket.emit('new-user', vm.currentUser);  //lets everyone else know that this user has joined
+    socket.emit(`new-user`, vm.currentUser);  //lets everyone else know that this user has joined
   }
 
-  socket.on('user-list', function(data) {
-  // This function expects a list of names with their socket ids
-    vm.users = data.users
-    $scope.$apply();
-  })
-
-  socket.on('add-new-user', function(user) {
+  socket.on(`${vm.currentUser.url}-add-new-user`, function(user) {
     //This expects an object with atleast a name and a socket id element
     vm.users.push(user);
     $scope.$apply();
