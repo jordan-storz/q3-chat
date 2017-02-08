8813fa5fc471b4317e594f5aa5956797c612aca0
jordan-storz <jordan.storz.1@gmail.com>
2017-02-08 14:31:56 -0700

    add message component options

--- src/app/single-message/single-message.component.js
+++ src/app/single-message/single-message.component.js
@@ -1,7 +1,21 @@
 import template from './single-message.template.html';
 
+const controller = [function () {
+  const vm = this;
+
+  vm.$onInit = function () {
+    vm.showOptions = false;
+  }
+
+  vm.toggleOptions = function () {
+    vm.showOptions = !vm.showOptions;
+  }
+}]
+
+
 module.exports = {
   template,
+  controller,
   bindings: {
     message: "="
   }
