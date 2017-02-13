d753b2b3266200e86a2da1752eb6fdc5b081d729
jordan-storz <jordan.storz.1@gmail.com>
2017-02-13 13:31:47 -0700

    change users http endpoint

--- src/app/services/user-http.service.js
+++ src/app/services/user-http.service.js
@@ -1,7 +1,7 @@
 module.exports = ['$http', function($http) {
   const service = this;
-  const endpoint = 'http://localhost:5200/api/users';
-
+  // const endpoint = 'http://localhost:5200/api/users';
+  const endpoint = 'https://mj-data.herokuapp.com/api/users';
   service.create = function (info) {
     return $http.post(endpoint, info);
   }
